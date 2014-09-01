5878c6949220c95a40e828d05fb18b3317751bfd
sambanshee <banshee.sodar@gmail.com>
2014-09-01 16:53:03 +0300

    second phase - editing and viewing are working now

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from bottle import *
 from google.appengine.api import users
 from google.appengine.ext import ndb
 from markdown2 import markdown
+import re
  
 app = Bottle()
 
@@ -18,24 +19,41 @@ class Posts(ndb.Model):
     title = ndb.StringProperty(indexed=True, required=True)
     #tags = ndb.StringProperty(required=True, repeated=True, choices=set(tags_list))
     tags = ndb.StringProperty(repeated=True, choices=set(tags_list))
+    images = ndb.StringProperty(repeated=True)
 
 @app.route('/')
-def main_page():
-   
+def main_page():  
     posts_query = Posts.query(ancestor=database_key).order(-Posts.date) 
-    posts = posts_query.fetch(10)   
-    output = template('posts.html', posts=posts)
+    posts = posts_query.fetch()   
+    output = template('posts.html', posts=posts, tags_list = tags_list)
     return output
 
+@app.route('/tags/<tag>')
+def main_page(tag):
+   
+    posts_query = Posts.query(Posts.tags==tag.decode('utf-8')).order(-Posts.date) 
+    posts = posts_query.fetch() 
+    if not posts:
+        redirect('/')
+    output = template('posts.html', posts=posts, tags_list = tags_list)
+    return output
+    
+@app.route('/tags/<keyword>')
+def main_page(keyword):
+   
+    posts_query = Posts.query(Posts.tags==tag.decode('utf-8')).order(-Posts.date) 
+    posts = posts_query.fetch() 
+    if not posts:
+        redirect('/')
+    output = template('posts.html', posts=posts, tags_list = tags_list)
+    return output
+
+    
 @app.route('/posts/<url_id>')
 def single_post(url_id):
     posts = ndb.Key(urlsafe=url_id)
     post = Posts.query(ancestor=posts).fetch()
-    #posts = { "post": post }
-    output = template('posts.html', posts=post)
-#    output =""
-#    for i  in post:
-#      output = output + post 
+    output = template('posts.html', posts=post, tags_list=tags_list)
     return output   
 
 @app.route('/sign', method='POST')
@@ -66,10 +84,51 @@ def add_article():
     for tag in tags:
       post_tags.append(tag.decode('utf-8'))
     post.tags = post_tags
+    photos = []
+    for photo_id in range(1,9):
+        photo = request.forms.get('photo_%s' % photo_id)
+        photos.append(photo)
+    #return post_tags
+    post.images = photos
+    post.put()
+    redirect('/')    
+
+@app.route('/edit/<url_id>')
+def edit_post(url_id):
+    user = users.get_current_user()
+    if not user:
+        redirect(users.create_login_url('/add'))
+    elif not users.is_current_user_admin():
+        redirect('/')
+    posts = ndb.Key(urlsafe=url_id)
+    post = Posts.query(ancestor=posts).fetch()
+    output = template('add.html', tags_list = tags_list, title = post[0].title, content = post[0].content, selection = post[0].tags, action="edit", photos=photos, url_id=url_id )
+    return output
+    
+@app.route('/edit', method='POST')
+def edit_post():
+    user = users.get_current_user()
+    if not user:
+        redirect(users.create_login_url('/add'))
+    elif not users.is_current_user_admin():
+        redirect('/')
+    
+    url_id = request.forms.get('url_id')
+    #return url_id
+    post = ndb.Key(urlsafe=url_id).get()
+    #post = Posts.query(ancestor=post_key).fetch()
+    post.content = request.forms.get('content')
+    post.title = request.forms.get('title')
+    tags = request.forms.getall('tags')
+    post_tags = []
+    for tag in tags:
+      post_tags.append(tag.decode('utf-8'))
+    post.tags = post_tags
     #return post_tags
     post.put()
     redirect('/')    
 
+    
 @app.route('/error')
 def error_page():  
   return "Error!"
